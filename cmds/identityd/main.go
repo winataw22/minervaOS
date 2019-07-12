cc5ff963680c4d18563aa0e069d25a1ad14110ca
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-07-12 11:15:57 +0200

    Squashed 3 commits from 2019-07-12
    - don't pass pointer to zbus interface
    - network: avoir recreating network resrouce namespace when it already
    - rename network.ExitIface to network.PubIface

--- cmds/identityd/main.go
+++ cmds/identityd/main.go
@@ -1,1 +1,84 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/052fad281e9493b22f06dce22bf98bbf
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/cenkalti/backoff"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/zosv2/modules/identity"
+)
+
+const seedPath = "/var/cache/seed.txt"
+
+func main() {
+	var (
+		tnodbURL string
+	)
+
+	flag.StringVar(&tnodbURL, "tnodb", "https://tnodb.dev.grid.tf", "address of tenant network object database")
+	flag.Parse()
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	nodeID, err := loadIdentity()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	farmID, err := identity.GetFarmID()
+	if err != nil {
+		log.Error().Err(err).Msg("fail to read farmer id from kernel parameters")
+		os.Exit(1)
+	}
+
+	store := identity.NewHTTPIDStore(tnodbURL)
+	f := func() error {
+		log.Info().Msg("start registration of the node")
+		if err := store.RegisterNode(nodeID, farmID); err != nil {
+			log.Error().Err(err).Msg("fail to register node identity")
+			return err
+		}
+		return nil
+	}
+
+	err = backoff.Retry(f, backoff.NewExponentialBackOff())
+	if err != nil {
+		return
+	}
+
+	log.Info().Msg("node registered successfully")
+}
+
+func loadIdentity() (identity.Identifier, error) {
+	if !exists(seedPath) {
+		log.Info().Msg("seed not found, generating new key pair")
+		nodeID, err := identity.GenerateKeyPair()
+		if err != nil {
+			log.Error().Err(err).Msg("fail to generate key pair for node identity")
+			return nil, err
+		}
+
+		if err := identity.SerializeSeed(nodeID, seedPath); err != nil {
+			log.Error().Err(err).Msg("fail to save identity seed on disk")
+			return nil, err
+		}
+	}
+
+	nodeID, err := identity.LoadSeed(seedPath)
+	if err != nil {
+		log.Error().Err(err).Msg("fail to save identity seed on disk")
+		return nil, err
+	}
+
+	log.Info().
+		Str("identify", nodeID.Identity()).
+		Msg("node identity loaded")
+	return nodeID, nil
+}
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
--- cmds/provisiond/main.go
+++ cmds/provisiond/main.go
@@ -1,1 +1,74 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/9e0c33ed62ad6d482a03d7dd9aeff7c3
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/zosv2/modules/identity"
+	"github.com/threefoldtech/zosv2/modules/provision"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	var (
+		msgBrokerCon string
+		resURL       string
+		tnodbURL     string
+		debug        bool
+	)
+
+	flag.StringVar(&msgBrokerCon, "broker", "unix:///var/run/redis.sock", "connection string to the message broker")
+	flag.StringVar(&tnodbURL, "tnodb", "https://tnodb.dev.grid.tf", "address of tenant network object database")
+	flag.StringVar(&resURL, "url", "", "reservation url to poll from")
+	flag.BoolVar(&debug, "debug", false, "enable debug logging")
+
+	if debug {
+		log.Logger.Level(zerolog.DebugLevel)
+	}
+
+	flag.Parse()
+
+	nodeID, err := identity.LocalNodeID()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to load node identity")
+	}
+
+	pipe, err := provision.FifoSource("/var/run/reservation.pipe")
+	if err != nil {
+		log.Fatal().Err(err).Msgf("failed to allocation reservation pipe")
+	}
+
+	source := pipe
+	if len(resURL) != 0 {
+		source = provision.CompinedSource(
+			pipe,
+			provision.HTTPSource(resURL, nodeID),
+		)
+	}
+
+	engine := provision.New(source)
+
+	// create context and add middlewares
+	ctx := context.Background()
+	client, err := zbus.NewRedisClient(msgBrokerCon)
+	if err != nil {
+		log.Fatal().Msgf("fail to connect to message broker server: %v", err)
+	}
+
+	ctx = provision.WithZBus(ctx, client)
+	ctx = provision.WithTnoDB(ctx, tnodbURL)
+
+	log.Info().
+		Str("broker", msgBrokerCon).
+		Msg("starting provision module")
+
+	if err := engine.Run(ctx); err != nil {
+		log.Error().Err(err).Msg("unexpected error")
+	}
+}
