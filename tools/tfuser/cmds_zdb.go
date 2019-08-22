d8393b4b5736d2c0342bef9eaa7859bfe9a9909e
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-08-22 14:10:19 +0200

    Squashed 8 commits from 2019-08-22
    - Merge pull request #196 from threefoldtech/zbd_provision
    - network: make ZDBIP a generic method to get ip address of any interface
    - Document storage paths under cache
    - Create notes_20190822.md
    - Update notes_20190822.md
    - add github action badge to home readme
    - unify storage directory flag and insinde zinit services
    - fix upgraded root storage

--- tools/tfuser/cmds_zdb.go
+++ tools/tfuser/cmds_zdb.go
@@ -1,1 +1,47 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/018291e856592810eda4aa08fed39005
+package main
+
+import (
+	"fmt"
+
+	"github.com/threefoldtech/zosv2/modules"
+
+	"github.com/threefoldtech/zosv2/modules/provision"
+	"github.com/urfave/cli"
+)
+
+func generateZDB(c *cli.Context) error {
+	var (
+		size     = c.Uint64("size")
+		mode     = c.String("mode")
+		password = c.String("password")
+		disktype = c.String("type")
+		public   = c.Bool("Public")
+	)
+
+	if disktype != modules.HDDDevice && disktype != modules.SSDDevice {
+		return fmt.Errorf("volume type can only 'HHD' or 'SSD'")
+	}
+
+	if mode != modules.ZDBModeSeq && mode != modules.ZDBModeUser {
+		return fmt.Errorf("mode can only 'user' or 'seq'")
+	}
+
+	if size < 1 { //TODO: upper bound ?
+		return fmt.Errorf("size cannot be less than 1")
+	}
+
+	zdb := provision.ZDB{
+		Size:     size,
+		DiskType: modules.DeviceType(disktype),
+		Mode:     modules.ZDBMode(mode),
+		Password: password,
+		Public:   public,
+	}
+
+	p, err := embed(zdb, provision.ZDBReservation)
+	if err != nil {
+		return err
+	}
+
+	return output(c.GlobalString("output"), p)
+}
