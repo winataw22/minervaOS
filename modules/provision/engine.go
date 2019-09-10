3c4b1e9866bfd0bbbad22a75757743b06690c263
Jan De Landtsheer <jan@delandtsheer.eu>
2019-09-10 08:29:36 +0200

    Notes20190909 (#237)
    * notes 20190909
    
    * date

--- modules/provision/engine.go
+++ modules/provision/engine.go
@@ -1,1 +1,169 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/56ee617beacd2c8bf5597f7876bd0538
+package provision
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/threefoldtech/zosv2/modules/stubs"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+)
+
+// ReservationReadWriter define the interface to store
+// some reservations
+type ReservationReadWriter interface {
+	Add(r *Reservation) error
+	Remove(id string) error
+}
+
+// Feedbacker defines the method that needs to be implemented
+// to send the provision result to BCDB
+type Feedbacker interface {
+	Feedback(id string, r *Result) error
+}
+
+type defaultEngine struct {
+	source ReservationSource
+	store  ReservationReadWriter
+	fb     Feedbacker
+}
+
+// New creates a new engine. Once started, the engine
+// will continue processing all reservations from the reservation source
+// and try to apply them.
+// the default implementation is a single threaded worker. so it process
+// one reservation at a time. On error, the engine will log the error. and
+// continue to next reservation.
+func New(source ReservationSource, rw ReservationReadWriter, fb Feedbacker) Engine {
+	return &defaultEngine{
+		source: source,
+		store:  rw,
+		fb:     fb,
+	}
+}
+
+// Run starts processing reservation resource. Then try to allocate
+// reservations
+func (e *defaultEngine) Run(ctx context.Context) error {
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	cReservation := e.source.Reservations(ctx)
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Info().Msg("provision engine context done, exiting")
+			return nil
+
+		case reservation, ok := <-cReservation:
+			if !ok {
+				log.Info().Msg("reservation source is emptied. stopping engine")
+				return nil
+			}
+
+			slog := log.With().
+				Str("id", string(reservation.ID)).
+				Str("type", string(reservation.Type)).
+				Str("duration", fmt.Sprintf("%v", reservation.Duration)).
+				Logger()
+
+			if !reservation.Expired() {
+				slog.Info().Msg("start provisioning reservation")
+				if err := e.provision(ctx, reservation); err != nil {
+					log.Error().Err(err).Msgf("failed to provision reservation %s", reservation.ID)
+				}
+			} else {
+				slog.Info().Msg("start decommissioning reservation")
+				if err := e.decommission(ctx, reservation); err != nil {
+					log.Error().Err(err).Msgf("failed to decommission reservation %s", reservation.ID)
+				}
+				slog.Info().Msg("reservation decommission successful")
+			}
+		}
+	}
+}
+
+func (e *defaultEngine) provision(ctx context.Context, r *Reservation) error {
+	if err := r.validate(); err != nil {
+		return errors.Wrapf(err, "failed validation of reservation")
+	}
+
+	fn, ok := provisioners[r.Type]
+	if !ok {
+		return fmt.Errorf("type of reservation not supported: %s", r.Type)
+	}
+
+	result, err := fn(ctx, r)
+
+	if replyErr := e.reply(ctx, r, err, result); replyErr != nil {
+		log.Error().Err(replyErr).Msg("failed to send result to BCDB")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if err := e.store.Add(r); err != nil {
+		return errors.Wrapf(err, "failed to cache reservation %s locally", r.ID)
+	}
+
+	return nil
+}
+
+func (e *defaultEngine) decommission(ctx context.Context, r *Reservation) error {
+	fn, ok := decommissioners[r.Type]
+	if !ok {
+		return fmt.Errorf("type of reservation not supported: %s", r.Type)
+	}
+
+	err := fn(ctx, r)
+	if err != nil {
+		errors.Wrap(err, "decommissioning of reservation failed")
+	}
+
+	if err := e.store.Remove(r.ID); err != nil {
+		errors.Wrapf(err, "failed to remove reservation %s from cache", r.ID)
+	}
+	return nil
+}
+
+func (e *defaultEngine) reply(ctx context.Context, r *Reservation, rErr error, info interface{}) error {
+	zbus := GetZBus(ctx)
+	identity := stubs.NewIdentityManagerStub(zbus)
+	result := &Result{}
+
+	if rErr != nil {
+		log.Error().
+			Err(rErr).
+			Str("id", r.ID).
+			Msgf("failed to apply provision")
+		result.Error = rErr.Error()
+	} else {
+		log.Info().
+			Str("result", fmt.Sprintf("%v", info)).
+			Msgf("workload deployed")
+	}
+
+	br, err := json.Marshal(info)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode result")
+	}
+	result.Data = br
+
+	b, err := result.Bytes()
+	if err != nil {
+		return errors.Wrap(err, "failed to convert the result to byte for signature")
+	}
+
+	sig, err := identity.Sign(b)
+	if err != nil {
+		return errors.Wrap(err, "failed to signed the result")
+	}
+	result.Signature = sig
+
+	return e.fb.Feedback(r.ID, result)
+}
--- modules/provision/remote_store.go
+++ modules/provision/remote_store.go
@@ -1,1 +1,131 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/9b434d6dc591c64e4bfe3662b9114af0
+package provision
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/threefoldtech/zosv2/modules"
+)
+
+// HTTPStore is a reservation store
+// over HTTP
+type HTTPStore struct {
+	baseURL string
+}
+
+// NewHTTPStore creates an a client to a TNoDB reachable over HTTP
+func NewHTTPStore(url string) *HTTPStore {
+	return &HTTPStore{baseURL: url}
+}
+
+// Reserve adds a reservation to the BCDB
+func (s *HTTPStore) Reserve(r *Reservation, nodeID modules.Identifier) error {
+	url := fmt.Sprintf("%s/reservations/%s", s.baseURL, nodeID.Identity())
+
+	buf := &bytes.Buffer{}
+
+	if err := json.NewEncoder(buf).Encode(r); err != nil {
+		return err
+	}
+	resp, err := http.Post(url, "application/json", buf)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("wrong response status code %s", resp.Status)
+	}
+
+	return nil
+}
+
+// Poll retrieves reservations from BCDB. If all is true, it returns all the reservations
+// for this node, otherwise is return only the reservation never sent yet and do long polling
+func (s *HTTPStore) Poll(nodeID modules.Identifier, all bool) ([]*Reservation, error) {
+	u, err := url.Parse(fmt.Sprintf("%s/reservations/%s/poll", s.baseURL, nodeID.Identity()))
+	if err != nil {
+		return nil, err
+	}
+	if all {
+		q := u.Query()
+		q.Add("all", "true")
+		u.RawQuery = q.Encode()
+	}
+
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("reservation request returned: %s", resp.Status)
+	}
+
+	if resp.Header.Get("content-type") != "application/json" {
+		return nil, fmt.Errorf("reservation request returned '%s', expected 'application/json'", resp.Header.Get("content-type"))
+	}
+
+	reservations := []*Reservation{}
+	if err := json.NewDecoder(resp.Body).Decode(&reservations); err != nil {
+		return nil, err
+	}
+	return reservations, nil
+}
+
+// Get retrieves a single reservation using its ID
+func (s *HTTPStore) Get(id string) (*Reservation, error) {
+	url := fmt.Sprintf("%s/reservations/%s", s.baseURL, id)
+
+	r := &Reservation{}
+	resp, err := http.Get(url)
+	if err != nil {
+		return r, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return r, fmt.Errorf("wrong response status code %s", resp.Status)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(r); err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+// Feedback sends back the result of a provisioning to BCDB
+func (s *HTTPStore) Feedback(id string, r *Result) error {
+	url := fmt.Sprintf("%s/reservations/%s", s.baseURL, id)
+
+	buf := &bytes.Buffer{}
+
+	if err := json.NewEncoder(buf).Encode(r); err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("PUT", url, buf)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("wrong response status code %s", resp.Status)
+	}
+	return nil
+}
