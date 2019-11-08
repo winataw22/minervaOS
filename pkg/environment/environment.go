8f893f89e96058e5be2e6659e3fbdaba5615061a
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-11-08 12:06:15 +0100

    Squashed 2 commits from 2019-11-08
    - bcdb_mock: don't write response twice
    - schema: fix default value for Date type in MarshalJSON

--- pkg/environment/environment.go
+++ pkg/environment/environment.go
@@ -1,1 +1,155 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/c0956327c88047c4961e52c811d0a8bd
+package environment
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/threefoldtech/zos/pkg"
+
+	"github.com/threefoldtech/zos/pkg/kernel"
+)
+
+// Environment holds information about running environment of a node
+// it defines the different constant based on the running mode (dev, test, prod)
+type Environment struct {
+	RunningMode RunningMode
+
+	BcdbURL       string
+	BcdbNamespace string
+	BcdbPassword  string
+
+	FarmerID pkg.FarmID
+	Orphan   bool
+
+	// ProvisionTimeout  int64
+	// ProvisionInterval int64
+}
+
+// RunningMode type
+type RunningMode string
+
+// Possible running mode of a node
+const (
+	RunningDev  RunningMode = "dev"
+	RunningTest RunningMode = "test"
+	RunningMain RunningMode = "prod"
+
+	// Orphanage is the default farmid where nodes are registered
+	// if no farmid were specified on the kernel command line
+	OrphanageDev  pkg.FarmID = 0
+	OrphanageTest pkg.FarmID = 0
+	OrphanageMain pkg.FarmID = 0
+)
+
+var (
+	envDev = Environment{
+		RunningMode: RunningDev,
+		BcdbURL:     "https://explorer.devnet.grid.tf",
+		// ProvisionTimeout:  60,
+		// ProvisionInterval: 10,
+	}
+
+	envTest = Environment{
+		RunningMode:   RunningTest,
+		BcdbURL:       "tcp://explorer.testnet.grid.tf:8901",
+		BcdbNamespace: "default",
+		// ProvisionTimeout:  120,
+		// ProvisionInterval: 10,
+	}
+
+	envProd = Environment{
+		RunningMode:   RunningMain,
+		BcdbURL:       "tcp://172.17.0.2:8901", //TODO: change once BCDB is online
+		BcdbNamespace: "default",
+		// ProvisionTimeout:  240,
+		// ProvisionInterval: 20,
+	}
+)
+
+// Get return the running environment of the node
+func Get() (Environment, error) {
+	params := kernel.GetParams()
+	return getEnvironmentFromParams(params)
+}
+
+func getEnvironmentFromParams(params kernel.Params) (Environment, error) {
+	var runmode []string
+	var env Environment
+
+	runmode, found := params.Get("runmode")
+	if !found || len(runmode) < 1 {
+		// Fallback to default production mode
+		runmode = []string{string(RunningMain)}
+	}
+
+	switch RunningMode(runmode[0]) {
+	case RunningDev:
+		env = envDev
+	case RunningTest:
+		env = envTest
+	case RunningMain:
+		env = envProd
+	default:
+		env = envProd
+	}
+
+	if RunningMode(runmode[0]) == RunningDev {
+		//allow override of the bcdb url in dev mode
+		bcdb, found := params.Get("bcdb")
+		if found && len(bcdb) >= 1 {
+			env.BcdbURL = bcdb[0]
+		}
+	}
+
+	farmerID, found := params.Get("farmer_id")
+	if !found || len(farmerID) < 1 || farmerID[0] == "" {
+		// fmt.Println("Warning: no valid farmer_id found in kernel parameter, fallback to orphanage")
+		env.Orphan = true
+
+		switch env.RunningMode {
+		case RunningDev:
+			env.FarmerID = OrphanageDev
+		case RunningTest:
+			env.FarmerID = OrphanageTest
+		case RunningMain:
+			env.FarmerID = OrphanageMain
+		}
+
+	} else {
+		env.Orphan = false
+		id, err := strconv.ParseUint(farmerID[0], 10, 64)
+		if err != nil {
+			return env, err
+		}
+		env.FarmerID = pkg.FarmID(id)
+	}
+
+	// Checking if there environment variable
+	// override default settings
+
+	if e := os.Getenv("ZOS_BCDB_URL"); e != "" {
+		env.BcdbURL = e
+	}
+
+	if e := os.Getenv("ZOS_BCDB_NAMESPACE"); e != "" {
+		env.BcdbNamespace = e
+	}
+
+	if e := os.Getenv("ZOS_BCDB_PASSWORD"); e != "" {
+		env.BcdbPassword = e
+	}
+
+	// if e := os.Getenv("ZOS_PROVISION_INTERVAL"); e != "" {
+	// 	if i, err := strconv.ParseInt(e, 10, 64); err == nil {
+	// 		env.ProvisionInterval = i
+	// 	}
+	// }
+
+	// if e := os.Getenv("ZOS_PROVISION_TIMEOUT"); e != "" {
+	// 	if i, err := strconv.ParseInt(e, 10, 64); err == nil {
+	// 		env.ProvisionTimeout = i
+	// 	}
+	// }
+
+	return env, nil
+}
--- pkg/identity/store.go
+++ pkg/identity/store.go
@@ -1,1 +1,97 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/5cbd4255a3bcdea01ae9299712d79934
+package identity
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/threefoldtech/zos/pkg/gedis/types/directory"
+
+	"github.com/threefoldtech/zos/pkg/geoip"
+
+	"github.com/threefoldtech/zos/pkg"
+)
+
+// IDStore is the interface defining the
+// client side of an identity store
+type IDStore interface {
+	RegisterNode(node pkg.Identifier, farm pkg.FarmID, version string, loc geoip.Location) (string, error)
+	RegisterFarm(tid uint64, name string, email string, wallet []string) (pkg.FarmID, error)
+}
+
+type httpIDStore struct {
+	baseURL string
+}
+
+// NewHTTPIDStore returns a HTTP IDStore client
+func NewHTTPIDStore(url string) IDStore {
+	return &httpIDStore{baseURL: url}
+}
+
+// RegisterNode implements the IDStore interface
+func (s *httpIDStore) RegisterNode(node pkg.Identifier, farm pkg.FarmID, version string, loc geoip.Location) (string, error) {
+	buf := bytes.Buffer{}
+	err := json.NewEncoder(&buf).Encode(directory.TfgridNode2{
+		NodeID:    node.Identity(),
+		FarmID:    uint64(farm),
+		OsVersion: version,
+		Location: directory.TfgridLocation1{
+			City:      loc.City,
+			Country:   loc.Country,
+			Continent: loc.Continent,
+			Latitude:  loc.Latitude,
+			Longitude: loc.Longitute,
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.Post(s.baseURL+"/nodes", "application/json", &buf)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode != http.StatusCreated {
+		return "", fmt.Errorf("wrong response status code received: %v", resp.Status)
+	}
+
+	return "(unknown)", nil
+}
+
+type farmRegisterReq struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// RegisterFarm implements the IDStore interface
+func (s *httpIDStore) RegisterFarm(tid uint64, name string, email string, wallet []string) (pkg.FarmID, error) {
+	buf := bytes.Buffer{}
+	err := json.NewEncoder(&buf).Encode(directory.TfgridFarm1{
+		ThreebotID:      tid,
+		Name:            name,
+		Email:           email,
+		WalletAddresses: wallet,
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := http.Post(s.baseURL+"/farms", "application/json", &buf)
+	if err != nil {
+		return 0, err
+	}
+	if resp.StatusCode != http.StatusCreated {
+		return 0, fmt.Errorf("wrong response status code received: %v", resp.Status)
+	}
+
+	defer resp.Body.Close()
+	id := struct {
+		ID pkg.FarmID `json:"id"`
+	}{}
+	if err := json.NewDecoder(resp.Body).Decode(&id); err != nil {
+		return 0, err
+	}
+
+	return id.ID, nil
+}
