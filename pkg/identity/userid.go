7a55ec9b63548e0c196cd95d16c92ba9182fd08a
Christophe de Carvalho <zaibon@users.noreply.github.com>
2020-04-06 16:30:53 +0200

    Squashed 7 commits from 2020-04-06
    - provisiond: add missing PublicIp6 in container workload converstion code
    - provisiond: sort the workloads from a reservation before sending it to
    - Merge pull request #678 from threefoldtech/escrow_rework
    - Merge branch 'explorer_metrics'
    - Merge remote-tracking branch 'origin/feature_stellar_logs'
    - explorer: try to detect empty addresses
    - Merge pull request #673 from threefoldtech/tools-seed

---
+++ pkg/identity/userid.go
@@ -0,0 +1,1 @@
+/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/3174e3f8486f104e94cf8d1ba04f36f9
--- tools/explorer/pkg/stellar/stellar.go
+++ tools/explorer/pkg/stellar/stellar.go
@@ -61,6 +61,13 @@ type Wallet struct {
 	asset   assetCodeEnum
 }
 
+// PayoutInfo holds information about which address needs to receive how many funds
+// for payment commands which take multiple receivers
+type PayoutInfo struct {
+	Address string
+	Amount  xdr.Int64
+}
+
 // New from seed
 func New(seed string, network string, asset string) (*Wallet, error) {
 	kp, err := keypair.ParseFull(seed)
@@ -156,6 +163,13 @@ func (w *Wallet) KeyPairFromSeed(seed string) (*keypair.Full, error) {
 // GetBalance gets balance for an address and a given reservation id. It also returns
 // a list of addresses which funded the given address.
 func (w *Wallet) GetBalance(address string, id schema.ID) (xdr.Int64, []string, error) {
+
+	if address == "" {
+		err := fmt.Errorf("trying to get the balance of an empty address. this should never happen")
+		log.Warn().Err(err).Send()
+		return 0, nil, err
+	}
+
 	var total xdr.Int64
 	horizonClient, err := w.getHorizonClient()
 	if err != nil {
@@ -169,6 +183,7 @@ func (w *Wallet) GetBalance(address string, id schema.ID) (xdr.Int64, []string, 
 		Cursor:     cursor,
 	}
 
+	log.Info().Str("address", address).Msg("fetching balance for address")
 	txes, err := horizonClient.Transactions(txReq)
 	if err != nil {
 		return 0, nil, errors.Wrap(err, "could not get transactions")
@@ -221,6 +236,7 @@ func (w *Wallet) GetBalance(address string, id schema.ID) (xdr.Int64, []string, 
 			cursor = tx.PagingToken()
 		}
 		txReq.Cursor = cursor
+		log.Info().Str("address", address).Msgf("fetching balance for address with cursor: %s", cursor)
 		txes, err = horizonClient.Transactions(txReq)
 		if err != nil {
 			return 0, nil, errors.Wrap(err, "could not get transactions")
@@ -289,11 +305,11 @@ func (w *Wallet) Refund(keypair keypair.Full, id schema.ID) error {
 	return nil
 }
 
-// PayoutFarmer using a keypair
+// PayoutFarmers using a keypair
 // keypair is account assiociated with farmer - user
 // destination is the farmer destination address
 // id is the reservation ID to pay for
-func (w *Wallet) PayoutFarmer(keypair keypair.Full, destination string, amount xdr.Int64, id schema.ID) error {
+func (w *Wallet) PayoutFarmers(keypair keypair.Full, destinations []PayoutInfo, id schema.ID) error {
 	sourceAccount, err := w.getAccountDetails(keypair.Address())
 	if err != nil {
 		return errors.Wrap(err, "failed to get source account")
@@ -302,35 +318,46 @@ func (w *Wallet) PayoutFarmer(keypair keypair.Full, destination string, amount x
 	if err != nil {
 		return errors.Wrap(err, "failed to get balance")
 	}
-	if balance < amount {
+	requiredAmount := xdr.Int64(0)
+	for _, pi := range destinations {
+		requiredAmount += pi.Amount
+	}
+	if balance < requiredAmount {
 		return ErrInsuficientBalance
 	}
 
-	// 10% cut for the foundation
-	/*
-		Based on the way we calculate the cost of reservation we know it has at most
-		6 digit precision whereas stellar has 7 digits precision.
-		This means that any valid reservation must necessarily have a "0" as least
-		significant digit (when expressed as `stropes` as is the case here).
-		With this knowledge it is safe to perform the 90% cut as regular integer operations
-		instead of using floating points which might lead to floating point errors
-	*/
-	if amount%10 != 0 {
-		return errors.New("invalid reservation cost")
-	}
-	foundationCut := amount / 10 * 1
-	amountDue := amount / 10 * 9
+	paymentOps := make([]txnbuild.Operation, 0, len(destinations)+1)
+	foundationCut := xdr.Int64(0)
 
-	farmerPaymentOP := txnbuild.Payment{
-		Destination: destination,
-		Amount:      big.NewRat(int64(amountDue), stellarPrecision).FloatString(stellarPrecisionDigits),
-		Asset: txnbuild.CreditAsset{
-			Code:   w.asset.String(),
-			Issuer: w.getIssuer(),
-		},
-		SourceAccount: &sourceAccount,
+	for _, pi := range destinations {
+		// 10% cut for the foundation
+		/*
+			Based on the way we calculate the cost of reservation we know it has at most
+			6 digit precision whereas stellar has 7 digits precision.
+			This means that any valid reservation must necessarily have a "0" as least
+			significant digit (when expressed as `stropes` as is the case here).
+			With this knowledge it is safe to perform the 90% cut as regular integer operations
+			instead of using floating points which might lead to floating point errors
+		*/
+		if pi.Amount%10 != 0 {
+			return errors.New("invalid reservation cost")
+		}
+		foundationCut += pi.Amount / 10 * 1
+		amountDue := pi.Amount / 10 * 9
+
+		paymentOps = append(paymentOps, &txnbuild.Payment{
+			Destination: pi.Address,
+			Amount:      big.NewRat(int64(amountDue), stellarPrecision).FloatString(stellarPrecisionDigits),
+			Asset: txnbuild.CreditAsset{
+				Code:   w.asset.String(),
+				Issuer: w.getIssuer(),
+			},
+			SourceAccount: &sourceAccount,
+		})
 	}
-	foundationPaymentOP := txnbuild.Payment{
+
+	// add foundation payment
+	paymentOps = append(paymentOps, &txnbuild.Payment{
 		Destination: w.keypair.Address(),
 		Amount:      big.NewRat(int64(foundationCut), stellarPrecision).FloatString(stellarPrecisionDigits),
 		Asset: txnbuild.CreditAsset{
@@ -338,12 +365,12 @@ func (w *Wallet) PayoutFarmer(keypair keypair.Full, destination string, amount x
 			Issuer: w.getIssuer(),
 		},
 		SourceAccount: &sourceAccount,
-	}
+	})
 
 	formattedMemo := fmt.Sprintf("%d", id)
 	memo := txnbuild.MemoText(formattedMemo)
 	tx := txnbuild.Transaction{
-		Operations: []txnbuild.Operation{&farmerPaymentOP, &foundationPaymentOP},
+		Operations: paymentOps,
 		Timebounds: txnbuild.NewTimeout(300),
 		Network:    w.getNetworkPassPhrase(),
 		Memo:       memo,
@@ -404,6 +431,7 @@ func (w *Wallet) signAndSubmitTx(keypair *keypair.Full, tx *txnbuild.Transaction
 		return errors.Wrap(err, "failed to sign transaction with keypair")
 	}
 
+	log.Info().Msg("submitting transaction to the stellar network")
 	// Submit the transaction
 	_, err = client.SubmitTransaction(*tx)
 	if err != nil {
@@ -422,8 +450,12 @@ func (w *Wallet) getAccountDetails(address string) (account hProtocol.Account, e
 		return hProtocol.Account{}, err
 	}
 	ar := horizonclient.AccountRequest{AccountID: address}
+	log.Info().Str("address", address).Msgf("fetching account details for address: ")
 	account, err = client.AccountDetail(ar)
-	return
+	if err != nil {
+		return hProtocol.Account{}, errors.Wrapf(err, "failed to get account details for account: %s", address)
+	}
+	return account, nil
 }
 
 func (w *Wallet) getHorizonClient() (*horizonclient.Client, error) {
