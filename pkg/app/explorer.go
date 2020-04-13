00eb4acc598dad39b34106e02bbb155769077c15
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-04-13 15:58:39 +0200

    Squashed 6 commits from 2020-04-13
    - Add a constructor for btrfs utils
    - Merge pull request #708 from threefoldtech/container-cleanup
    - provisiond: report the number of workloads provisionned (#705)
    - explorer pass proper router to workloads.Setup
    - keep track of storage units as bytes to allow counting part of a unit  (#710)
    - phonebook: make sure all threebot name are lower case

--- pkg/app/explorer.go
+++ pkg/app/explorer.go
@@ -1,1 +1,47 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/329c1a0d8191d86d43e53f9cef6715ac
+package app
+
+import (
+	"crypto/ed25519"
+
+	"github.com/pkg/errors"
+	"github.com/threefoldtech/zos/pkg/environment"
+	"github.com/threefoldtech/zos/pkg/identity"
+	"github.com/threefoldtech/zos/tools/client"
+)
+
+const seedPath = "/var/cache/modules/identityd/seed.txt"
+
+// ExplorerClient return the client to the explorer based
+// on the environment configured in the kernel arguments
+func ExplorerClient() (*client.Client, error) {
+	env, err := environment.Get()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse node environment")
+	}
+
+	kp, err := identity.LoadKeyPair(seedPath)
+	if err != nil {
+		return nil, err
+	}
+
+	cl, err := client.NewClient(env.BcdbURL, nodeIdentity{
+		kp: kp,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return cl, nil
+}
+
+type nodeIdentity struct {
+	kp identity.KeyPair
+}
+
+func (n nodeIdentity) PrivateKey() ed25519.PrivateKey {
+	return n.kp.PrivateKey
+}
+
+func (n nodeIdentity) Identity() string {
+	return n.kp.Identity()
+}
