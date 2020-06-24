27ada12cffb16cf0afeaf60c55169030455f43d1
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-06-24 15:49:34 +0200

    Squashed 5 commits from 2020-06-24
    - Merge pull request #829 from threefoldtech/extra-bins-corex
    - networkd: close zinit connection after yggdrasil is stopped
    - Merge pull request #837 from threefoldtech/master-volume-type
    - Revert "Merge pull request #837 from threefoldtech/master-volume-type"
    - networkd: split the method used to wait for IP for zdb and container

--- pkg/provision/primitives/provisioner.go
+++ pkg/provision/primitives/provisioner.go
@@ -1,1 +1,48 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/206a0ca67cfd23bb425187f8592f8e27
+package primitives
+
+import (
+	"context"
+	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/zos/pkg/provision"
+)
+
+// Provisioner hold all the logic responsible to provision and decomission
+// the different primitives workloads defined by this package
+type Provisioner struct {
+	cache provision.ReservationCache
+	zbus  zbus.Client
+
+	Provisioners    map[provision.ReservationType]provision.ProvisionerFunc
+	Decommissioners map[provision.ReservationType]provision.DecomissionerFunc
+}
+
+// NewProvisioner creates a new 0-OS provisioner
+func NewProvisioner(cache provision.ReservationCache, zbus zbus.Client) *Provisioner {
+	p := &Provisioner{
+		cache: cache,
+		zbus:  zbus,
+	}
+	p.Provisioners = map[provision.ReservationType]provision.ProvisionerFunc{
+		ContainerReservation:  p.containerProvision,
+		VolumeReservation:     p.volumeProvision,
+		NetworkReservation:    p.networkProvision,
+		ZDBReservation:        p.zdbProvision,
+		DebugReservation:      p.debugProvision,
+		KubernetesReservation: p.kubernetesProvision,
+	}
+	p.Decommissioners = map[provision.ReservationType]provision.DecomissionerFunc{
+		ContainerReservation:  p.containerDecommission,
+		VolumeReservation:     p.volumeDecommission,
+		NetworkReservation:    p.networkDecommission,
+		ZDBReservation:        p.zdbDecommission,
+		DebugReservation:      p.debugDecommission,
+		KubernetesReservation: p.kubernetesDecomission,
+	}
+
+	return p
+}
+
+// RuntimeUpgrade runs upgrade needed when provision daemon starts
+func (p *Provisioner) RuntimeUpgrade(ctx context.Context) {
+	p.upgradeRunningZdb(ctx)
+}
--- pkg/stubs/flist_stub.go
+++ pkg/stubs/flist_stub.go
@@ -1,1 +1,113 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/d56805469951d42450408a9b5eceac9c
+package stubs
+
+import (
+	zbus "github.com/threefoldtech/zbus"
+	pkg "github.com/threefoldtech/zos/pkg"
+)
+
+type FlisterStub struct {
+	client zbus.Client
+	module string
+	object zbus.ObjectID
+}
+
+func NewFlisterStub(client zbus.Client) *FlisterStub {
+	return &FlisterStub{
+		client: client,
+		module: "flist",
+		object: zbus.ObjectID{
+			Name:    "flist",
+			Version: "0.0.1",
+		},
+	}
+}
+
+func (s *FlisterStub) Mount(arg0 string, arg1 string, arg2 pkg.MountOptions) (ret0 string, ret1 error) {
+	args := []interface{}{arg0, arg1, arg2}
+	result, err := s.client.Request(s.module, s.object, "Mount", args...)
+	if err != nil {
+		panic(err)
+	}
+	if err := result.Unmarshal(0, &ret0); err != nil {
+		panic(err)
+	}
+	ret1 = new(zbus.RemoteError)
+	if err := result.Unmarshal(1, &ret1); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func (s *FlisterStub) NamedMount(arg0 string, arg1 string, arg2 string, arg3 pkg.MountOptions) (ret0 string, ret1 error) {
+	args := []interface{}{arg0, arg1, arg2, arg3}
+	result, err := s.client.Request(s.module, s.object, "NamedMount", args...)
+	if err != nil {
+		panic(err)
+	}
+	if err := result.Unmarshal(0, &ret0); err != nil {
+		panic(err)
+	}
+	ret1 = new(zbus.RemoteError)
+	if err := result.Unmarshal(1, &ret1); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func (s *FlisterStub) NamedUmount(arg0 string) (ret0 error) {
+	args := []interface{}{arg0}
+	result, err := s.client.Request(s.module, s.object, "NamedUmount", args...)
+	if err != nil {
+		panic(err)
+	}
+	ret0 = new(zbus.RemoteError)
+	if err := result.Unmarshal(0, &ret0); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func (s *FlisterStub) Umount(arg0 string) (ret0 error) {
+	args := []interface{}{arg0}
+	result, err := s.client.Request(s.module, s.object, "Umount", args...)
+	if err != nil {
+		panic(err)
+	}
+	ret0 = new(zbus.RemoteError)
+	if err := result.Unmarshal(0, &ret0); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func (s *FlisterStub) HashFromRootPath(arg0 string) (ret0 string, ret1 error) {
+	args := []interface{}{arg0}
+	result, err := s.client.Request(s.module, s.object, "HashFromRootPath", args...)
+	if err != nil {
+		panic(err)
+	}
+	if err := result.Unmarshal(0, &ret0); err != nil {
+		panic(err)
+	}
+	ret1 = new(zbus.RemoteError)
+	if err := result.Unmarshal(1, &ret1); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func (s *FlisterStub) FlistHash(arg0 string) (ret0 string, ret1 error) {
+	args := []interface{}{arg0}
+	result, err := s.client.Request(s.module, s.object, "FlistHash", args...)
+	if err != nil {
+		panic(err)
+	}
+	if err := result.Unmarshal(0, &ret0); err != nil {
+		panic(err)
+	}
+	ret1 = new(zbus.RemoteError)
+	if err := result.Unmarshal(1, &ret1); err != nil {
+		panic(err)
+	}
+	return
+}
