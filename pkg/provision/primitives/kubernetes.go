bfb9944f1d0cccd0a2801b1bc8f0cd15da682a6f
Lee Smet <lee.smet@hotmail.com>
2020-11-20 23:53:49 +0100

    Squashed 9 commits from 2020-11-20
    - bins: package: bump zufs to v2.0.7 (#1066)
    - Add public bridge to support public IPv4 reservations
    - Point tfexplorer dep in go.mod to public_ip branch
    - Remove zbus go.mod override
    - Comment newly exported function
    - Temporarily use a different k3os package
    - Make ci happy
    - Only create public bridge if it doesn't exist yet
    - Also use public bridge if it already exists

--- pkg/provision/primitives/kubernetes.go
+++ pkg/provision/primitives/kubernetes.go
@@ -9,7 +9,10 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/tfexplorer/models/generated/workloads"
+	"github.com/threefoldtech/tfexplorer/schema"
 	"github.com/threefoldtech/zos/pkg"
+	"github.com/threefoldtech/zos/pkg/app"
 	"github.com/threefoldtech/zos/pkg/provision"
 	"github.com/threefoldtech/zos/pkg/stubs"
 )
@@ -43,11 +46,14 @@ type Kubernetes struct {
 	// the later, the VM will retrieve the github keys for this username
 	// when it boots.
 	SSHKeys []string `json:"ssh_keys"`
+	// PublicIP points to a reservation for a public ip
+	PublicIP schema.ID `json:"public_ip"`
 
 	PlainClusterSecret string `json:"-"`
 }
 
-const k3osFlistURL = "https://hub.grid.tf/tf-official-apps/k3os.flist"
+// const k3osFlistURL = "https://hub.grid.tf/tf-official-apps/k3os.flist"
+const k3osFlistURL = "https://hub.grid.tf/lee/k3os.flist"
 
 func (p *Provisioner) kubernetesProvision(ctx context.Context, reservation *provision.Reservation) (interface{}, error) {
 	return p.kubernetesProvisionImpl(ctx, reservation)
@@ -81,6 +87,11 @@ func (p *Provisioner) kubernetesProvisionImpl(ctx context.Context, reservation *
 
 	if exists {
 		return result, errors.New("kubernetes vm with same network already exists")
+	}
+
+	// check if public ipv4 is supported, should this be requested
+	if config.PublicIP != 0 && !network.PublicIPv4Support() {
+		return result, errors.New("public ipv4 is requested, but not supported on this node")
 	}
 
 	result.ID = reservation.ID
@@ -148,8 +159,16 @@ func (p *Provisioner) kubernetesProvisionImpl(ctx context.Context, reservation *
 		}
 	}()
 
+	var pubIface string
+	if config.PublicIP != 0 {
+		pubIface, err = network.SetupPubTap(netID)
+		if err != nil {
+			return result, errors.Wrap(err, "could not set up tap device for public network")
+		}
+	}
+
 	var netInfo pkg.VMNetworkInfo
-	netInfo, err = p.buildNetworkInfo(ctx, reservation.User, iface, config)
+	netInfo, err = p.buildNetworkInfo(ctx, reservation.User, iface, pubIface, config)
 	if err != nil {
 		return result, errors.Wrap(err, "could not generate network info")
 	}
@@ -201,6 +220,7 @@ func (p *Provisioner) kubernetesInstall(ctx context.Context, name string, cpu ui
 		InitrdImage: imagePath + "/k3os-initrd-amd64",
 		KernelArgs:  cmdline,
 		Disks:       disks,
+		NoKeepAlive: true, //machine will not restarted automatically when it exists
 	}
 
 	if err := vm.Run(installVM); err != nil {
@@ -272,6 +292,12 @@ func (p *Provisioner) kubernetesDecomission(ctx context.Context, reservation *pr
 		return errors.Wrap(err, "could not clean up tap device")
 	}
 
+	if cfg.PublicIP != 0 {
+		if err := network.RemovePubTap(netID); err != nil {
+			return errors.Wrap(err, "could not clean up public tap device")
+		}
+	}
+
 	if err := storage.Deallocate(fmt.Sprintf("%s-%s", reservation.ID, "vda")); err != nil {
 		return errors.Wrap(err, "could not remove vDisk")
 	}
@@ -284,7 +310,7 @@ func (p *Provisioner) kubernetesDecomission(ctx context.Context, reservation *pr
 	return nil
 }
 
-func (p *Provisioner) buildNetworkInfo(ctx context.Context, userID string, iface string, cfg Kubernetes) (pkg.VMNetworkInfo, error) {
+func (p *Provisioner) buildNetworkInfo(ctx context.Context, userID string, iface string, pubIface string, cfg Kubernetes) (pkg.VMNetworkInfo, error) {
 	network := stubs.NewNetworkerStub(p.zbus)
 
 	netID := provision.NetworkID(userID, string(cfg.NetworkID))
@@ -297,25 +323,106 @@ func (p *Provisioner) buildNetworkInfo(ctx context.Context, userID string, iface
 		return pkg.VMNetworkInfo{}, fmt.Errorf("IP %s is not part of local nr subnet %s", cfg.IP.String(), subnet.String())
 	}
 
+	privNet, err := network.GetNet(netID)
+	if err != nil {
+		return pkg.VMNetworkInfo{}, errors.Wrapf(err, "could not get network range")
+	}
+
 	addrCIDR := net.IPNet{
 		IP:   cfg.IP,
 		Mask: subnet.Mask,
 	}
 
-	gw, err := network.GetDefaultGwIP(netID)
+	gw4, gw6, err := network.GetDefaultGwIP(netID)
 	if err != nil {
-		return pkg.VMNetworkInfo{}, errors.Wrapf(err, "could not get network resource default gateway")
+		return pkg.VMNetworkInfo{}, errors.Wrap(err, "could not get network resource default gateway")
+	}
+
+	privIP6, err := network.GetIPv6From4(netID, cfg.IP)
+	if err != nil {
+		return pkg.VMNetworkInfo{}, errors.Wrap(err, "could not convert private ipv4 to ipv6")
 	}
 
 	networkInfo := pkg.VMNetworkInfo{
-		Tap:         iface,
-		MAC:         "", // rely on static IP configuration so we don't care here
-		AddressCIDR: addrCIDR,
-		GatewayIP:   net.IP(gw),
+		Ifaces: []pkg.VMIface{{
+			Tap:            iface,
+			MAC:            "", // rely on static IP configuration so we don't care here
+			IP4AddressCIDR: addrCIDR,
+			IP4GatewayIP:   net.IP(gw4),
+			IP4Net:         privNet,
+			IP6AddressCIDR: privIP6,
+			IP6GatewayIP:   gw6,
+			Public:         false,
+		}},
 		Nameservers: []net.IP{net.ParseIP("8.8.8.8"), net.ParseIP("8.8.4.4")},
 	}
 
+	if cfg.PublicIP != 0 {
+		// A public ip is set, load the reservation, extract the ip and make a config
+		// for it
+		// TODO: proper firewalling on the host
+
+		pubIP, pubGw, err := p.getPubIPConfig(cfg.PublicIP)
+		if err != nil {
+			return pkg.VMNetworkInfo{}, errors.Wrap(err, "could not get public ip config")
+		}
+
+		iface := pkg.VMIface{
+			Tap:            pubIface,
+			MAC:            "", // static ip is set so not important
+			IP4AddressCIDR: pubIP,
+			IP4GatewayIP:   pubGw,
+			// for now we get ipv6 from slaac, so leave ipv6 stuffs this empty
+			//
+			Public: true,
+		}
+
+		networkInfo.Ifaces = append(networkInfo.Ifaces, iface)
+	}
+
 	return networkInfo, nil
+}
+
+// Get the public ip, and the gateway from the reservation ID
+func (p *Provisioner) getPubIPConfig(rid schema.ID) (net.IPNet, net.IP, error) {
+	// TODO: check if there is a better way to do this
+	explorerClient, err := app.ExplorerClient()
+	if err != nil {
+		return net.IPNet{}, nil, errors.Wrap(err, "could not create explorer client")
+	}
+
+	workloadDefinition, err := explorerClient.Workloads.Get(rid)
+	if err != nil {
+		return net.IPNet{}, nil, errors.Wrap(err, "could not load public ip reservation")
+	}
+	// load IP
+	ip, ok := workloadDefinition.(*workloads.PublicIP)
+	if !ok {
+		return net.IPNet{}, nil, errors.Wrap(err, "could not decode ip reservation")
+	}
+	identity := stubs.NewIdentityManagerStub(p.zbus)
+	self := identity.NodeID().Identity()
+	selfDescription, err := explorerClient.Directory.NodeGet(self, false)
+	if err != nil {
+		return net.IPNet{}, nil, errors.Wrap(err, "could not get our own node description")
+	}
+	farm, err := explorerClient.Directory.FarmGet(schema.ID(selfDescription.FarmId))
+	if err != nil {
+		return net.IPNet{}, nil, errors.Wrap(err, "could not get our own farm")
+	}
+
+	var pubGw schema.IP
+	for _, ips := range farm.IPAddresses {
+		if ips.ReservationID == rid {
+			pubGw = ips.Gateway
+			break
+		}
+	}
+	if pubGw.IP == nil {
+		return net.IPNet{}, nil, errors.New("unable to identify public ip gateway")
+	}
+
+	return ip.IPaddress.IPNet, pubGw.IP, nil
 }
 
 // returns the vCpu's, memory, disksize for a vm size
@@ -365,6 +472,15 @@ func vmSize(size uint8) (cpu uint8, memory uint64, storage uint64, err error) {
 	case 14:
 		//1 vCpu, 64 GiB RAM, 800 GB disk
 		return 1, 64 * 1024, 800 * 1024, nil
+	case 15:
+		//1 vCpu, 2 GiB RAM, 25 GB disk
+		return 1, 2 * 1024, 25 * 1024, nil
+	case 16:
+		//2 vCpu, 4 GiB RAM, 50 GB disk
+		return 2, 4 * 1024, 50 * 1024, nil
+	case 17:
+		//4 vCpu, 8 GiB RAM, 50 GB disk
+		return 4, 8 * 1024, 50 * 1024, nil
 	}
 
 	return 0, 0, 0, fmt.Errorf("unsupported vm size %d, only size 1 and 2 are supported", size)
