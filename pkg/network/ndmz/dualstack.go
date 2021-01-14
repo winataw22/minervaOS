c7e10c530aa3198524360e8002ca9f2ca75eb87d
Lee Smet <lee.smet@hotmail.com>
2021-01-14 15:26:31 +0100

    Squashed 2 commits from 2021-01-14
    - Fix #1149: Sort out host ipv6 if br-pub is present (#1151)
    - Improve networking flow

--- pkg/network/ndmz/dualstack.go
+++ pkg/network/ndmz/dualstack.go
@@ -46,7 +46,7 @@ func NewDualStack(nodeID string, master string) *DualStack {
 	}
 }
 
-//Create create the NDMZ network namespace and configure its default routes and addresses
+// Create create the NDMZ network namespace and configure its default routes and addresses
 func (d *DualStack) Create(ctx context.Context) error {
 	master := d.ipv6Master
 	var err error
@@ -322,10 +322,6 @@ func waitIP6() error {
 	if err := ifaceutil.SetLoUp(); err != nil {
 		return errors.Wrapf(err, "ndmz: couldn't bring lo up in ndmz namespace")
 	}
-	// first, disable forwarding, so we can get an IPv6 deft route on public from an RA
-	if _, err := sysctl.Sysctl("net.ipv6.conf.all.forwarding", "0"); err != nil {
-		return errors.Wrapf(err, "ndmz: failed to disable ipv6 forwarding in ndmz namespace")
-	}
 	// also, set kernel parameter that public always accepts an ra even when forwarding
 	if _, err := sysctl.Sysctl(fmt.Sprintf("net.ipv6.conf.%s.accept_ra", DMZPub6), "2"); err != nil {
 		return errors.Wrapf(err, "ndmz: failed to accept_ra=2 in ndmz namespace")
@@ -340,13 +336,11 @@ func waitIP6() error {
 		return errors.Wrapf(err, "ndmz: couldn't disable proxy-arp on %s in ndmz namespace", DMZPub6)
 	}
 
-	var routes []netlink.Route
 	getRoutes := func() (err error) {
 		log.Info().Msg("wait for slaac to give ipv6")
 		// check if in the mean time SLAAC gave us an IPv6 deft gw, save it, and reapply after enabling forwarding
 		checkipv6 := net.ParseIP("2606:4700:4700::1111")
-		routes, err = netlink.RouteGet(checkipv6)
-		if err != nil {
+		if _, err = netlink.RouteGet(checkipv6); err != nil {
 			return errors.Wrapf(err, "ndmz: failed to get the IPv6 routes in ndmz")
 		}
 		return nil
@@ -354,14 +348,5 @@ func waitIP6() error {
 
 	bo := backoff.NewExponentialBackOff()
 	bo.MaxElapsedTime = 122 * time.Second // default RA from router is every 60 secs
-	if err := backoff.Retry(getRoutes, bo); err != nil {
-		return err
-	}
-
-	if len(routes) == 1 {
-		if _, err := sysctl.Sysctl("net.ipv6.conf.all.forwarding", "1"); err != nil {
-			return errors.Wrapf(err, "ndmz: failed to enable ipv6 forwarding in ndmz namespace")
-		}
-	}
-	return nil
+	return backoff.Retry(getRoutes, bo)
 }
