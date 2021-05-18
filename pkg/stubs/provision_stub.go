e61ea1c62ba9e45f6b1a40c4c462cbcdb770c6d7
Muhamad Azmy <muhamad@incubaid.com>
2021-05-18 11:21:39 +0200

    Zos3/unify size unit (#1273)
    * WIP: use a new type Unit to unify sizes
    
    A unit currently = 1 byte of either memory or storage
    this will make it either for calculating usage and capacity
    
    * correct response status
    
    * add logs to provisiond
    
    this was needed to help debug issue with redirections
    
    * use correct values for reserved cap
    
    * WiP: mem mgmt
    
    * finalize memoy mgmt
    
    plus multiple fixes
    
    * fix ci
    
    * more ci
    
    * ci
    
    * fix all tests

--- pkg/stubs/provision_stub.go
+++ pkg/stubs/provision_stub.go
@@ -1,1 +1,56 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/4faa2409e01b98e76e395a5325578c34
+package stubs
+
+import (
+	"context"
+	zbus "github.com/threefoldtech/zbus"
+	pkg "github.com/threefoldtech/zos/pkg"
+)
+
+type ProvisionStub struct {
+	client zbus.Client
+	module string
+	object zbus.ObjectID
+}
+
+func NewProvisionStub(client zbus.Client) *ProvisionStub {
+	return &ProvisionStub{
+		client: client,
+		module: "provision",
+		object: zbus.ObjectID{
+			Name:    "provision",
+			Version: "0.0.1",
+		},
+	}
+}
+
+func (s *ProvisionStub) Counters(ctx context.Context) (<-chan pkg.ProvisionCounters, error) {
+	ch := make(chan pkg.ProvisionCounters)
+	recv, err := s.client.Stream(ctx, s.module, s.object, "Counters")
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer close(ch)
+		for event := range recv {
+			var obj pkg.ProvisionCounters
+			if err := event.Unmarshal(&obj); err != nil {
+				panic(err)
+			}
+			ch <- obj
+		}
+	}()
+	return ch, nil
+}
+
+func (s *ProvisionStub) DecommissionCached(ctx context.Context, arg0 string, arg1 string) (ret0 error) {
+	args := []interface{}{arg0, arg1}
+	result, err := s.client.RequestContext(ctx, s.module, s.object, "DecommissionCached", args...)
+	if err != nil {
+		panic(err)
+	}
+	ret0 = new(zbus.RemoteError)
+	if err := result.Unmarshal(0, &ret0); err != nil {
+		panic(err)
+	}
+	return
+}
