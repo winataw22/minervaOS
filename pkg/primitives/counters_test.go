d6272eeae75e406a47bcde7499d1273bd439b56e
OmarElawady <omarelawady1998@gmail.com>
2021-05-19 11:13:10 +0200

    update mrus and srus to be in bytes (#1277)
    * update mrus and srus to be in bytes
    
    * update vm validation

--- pkg/primitives/counters_test.go
+++ pkg/primitives/counters_test.go
@@ -1,1 +1,32 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/8649b9aa0983a8fe0ab097e449016d0a
+package primitives
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+)
+
+func TestCounterDecrement(t *testing.T) {
+	t.Run("regular_decrement", func(t *testing.T) {
+		counter := AtomicUnit(0)
+		counter.Increment(5)
+		dec := counter.Decrement(3)
+		assert.Equal(t, dec, gridtypes.Unit(2))
+		assert.Equal(t, counter.Current(), gridtypes.Unit(2))
+	})
+	t.Run("decrement_to_zero", func(t *testing.T) {
+		counter := AtomicUnit(0)
+		counter.Increment(5)
+		dec := counter.Decrement(5)
+		assert.Equal(t, dec, gridtypes.Unit(0))
+		assert.Equal(t, counter.Current(), gridtypes.Unit(0))
+	})
+	t.Run("decrement_negative", func(t *testing.T) {
+		counter := AtomicUnit(0)
+		counter.Increment(5)
+		dec := counter.Decrement(7)
+		assert.Equal(t, dec, gridtypes.Unit(0))
+		assert.Equal(t, counter.Current(), gridtypes.Unit(0))
+	})
+}
