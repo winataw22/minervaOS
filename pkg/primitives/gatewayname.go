f695894a941d76210c22b81ee00a75169b6579eb
OmarElawady <omarelawady1998@gmail.com>
2021-09-20 12:10:05 +0200

    Add dns resolution and wildcard certificate generation (#1389)
    * WIP: add dns resolution and wildcard certificate generation
    
    * fix dnsmasq config
    
    * typo fix
    
    * add envs to zinit
    
    * fix cert perms and conditions

--- pkg/primitives/gatewayname.go
+++ pkg/primitives/gatewayname.go
@@ -1,1 +1,62 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/48fa5a73a63a7c6119170a72ae104785
+package primitives
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
+	"github.com/threefoldtech/zos/pkg/provision"
+	"github.com/threefoldtech/zos/pkg/stubs"
+)
+
+func validateNameContract(twinID uint32, name string) error {
+	// TODO: validate against substrate?
+	return nil
+}
+
+func (p *Primitives) gwProvision(ctx context.Context, wl *gridtypes.WorkloadWithID) (interface{}, error) {
+	result := zos.GatewayProxyResult{}
+	var proxy zos.GatewayNameProxy
+	if err := json.Unmarshal(wl.Data, &proxy); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal gateway proxy from reservation: %w", err)
+	}
+	backends := make([]string, len(proxy.Backends))
+	for idx, backend := range proxy.Backends {
+		backends[idx] = string(backend)
+	}
+	// what we need to do:
+	// - does this node support gateways ?
+	// this can be validated by checking if we have a "public" namespace
+	deployment := provision.GetDeployment(ctx)
+	twinID := deployment.TwinID
+	if err := validateNameContract(twinID, proxy.Name); err != nil {
+		return nil, errors.Wrap(err, "failed to validate name contract")
+	}
+	// - Validation of ownership of the name (later)
+	// this must be done against substrate. Make sure that same user (twin) owns the
+	// name int he workload config
+
+	// - make necessary calls to gateway daemon.
+	// gateway := stubs.NewGatewayStub(p.zbus)
+	// gateway.SetNamedProxy(ctx context.Context, arg0 string, arg1 []string)
+	gateway := stubs.NewGatewayStub(p.zbus)
+	fqdn, err := gateway.SetNamedProxy(ctx, wl.ID.String(), proxy.Name, backends, proxy.TLSPassthrough)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to setup name proxy")
+	}
+	result.FQDN = fqdn
+	log.Debug().Str("domain", fqdn).Msg("domain reserved")
+	return result, nil
+}
+
+func (p *Primitives) gwDecommission(ctx context.Context, wl *gridtypes.WorkloadWithID) error {
+	gateway := stubs.NewGatewayStub(p.zbus)
+	if err := gateway.DeleteNamedProxy(ctx, wl.ID.String()); err != nil {
+		return errors.Wrap(err, "failed to delete name proxy")
+	}
+	return nil
+}
