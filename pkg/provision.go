81d8b3fc83647f4f16e23dbd8e897c86c39734f4
Waleed <waleed.hammam@gmail.com>
2021-12-01 17:09:23 +0200

    add optional parameter for redis pool size in rmb client (#1494)

--- pkg/provision.go
+++ pkg/provision.go
@@ -1,1 +1,20 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/80895e15f340c706b3e899e7fe136aa7
+package pkg
+
+//go:generate zbusc -module provision -version 0.0.1 -name provision -package stubs github.com/threefoldtech/zos/pkg+Provision stubs/provision_stub.go
+//go:generate zbusc -module provision -version 0.0.1 -name statistics -package stubs github.com/threefoldtech/zos/pkg+Statistics stubs/statistics_stub.go
+
+import (
+	"context"
+
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+)
+
+
+// Provision interface
+type Provision interface {
+	DecommissionCached(id string, reason string) error
+}
+
+type Statistics interface {
+	Reserved(ctx context.Context) <-chan gridtypes.Capacity
+}
