c7330bc7d99396b5124b218c4c336aca102b3114
Muhamad Azamy <muhamad@incubaid.com>
2022-02-25 11:51:35 +0100

    Squashed 2 commits from 2022-02-25
    - improvement on certify tool
    - latest substrate client

--- cmds/modules/noded/public.go
+++ cmds/modules/noded/public.go
@@ -1,1 +1,77 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/6386c4047a2f1df39167cfed683a6d8f
+package noded
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/substrate-client"
+	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/zos/pkg"
+	"github.com/threefoldtech/zos/pkg/environment"
+	"github.com/threefoldtech/zos/pkg/stubs"
+)
+
+func setPublicConfig(ctx context.Context, cl zbus.Client, cfg substrate.PublicConfig) error {
+	log.Info().Msg("setting node public config")
+	netMgr := stubs.NewNetworkerStub(cl)
+
+	pub, err := pkg.PublicConfigFrom(cfg)
+	if err != nil {
+		return errors.Wrap(err, "failed to create public config from setup")
+	}
+
+	return netMgr.SetPublicConfig(ctx, pub)
+}
+
+// public sets and watches changes to public config on chain and tries to apply the provided setup
+func public(ctx context.Context, nodeID uint32, env environment.Environment, cl zbus.Client) error {
+	mgr, err := environment.GetSubstrate()
+	if err != nil {
+		return err
+	}
+
+	stub := stubs.NewEventsStub(cl)
+	events, err := stub.PublicConfigEvent(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to subscribe to node events")
+	}
+
+	getNode := func() (*substrate.Node, error) {
+		sub, err := mgr.Substrate()
+		if err != nil {
+			return nil, err
+		}
+
+		defer sub.Close()
+		return sub.GetNode(nodeID)
+	}
+
+reapply:
+	for {
+		node, err := getNode()
+		if err != nil {
+			return errors.Wrap(err, "failed to get node public config")
+		}
+
+		if node.PublicConfig.HasValue {
+			if err := setPublicConfig(ctx, cl, node.PublicConfig.AsValue); err != nil {
+				return errors.Wrap(err, "failed to ")
+			}
+		}
+
+		for event := range events {
+			if event.Kind == pkg.EventSubscribed {
+				// the events has re-subscribed, so possible
+				// loss of events.
+				// then we-reapply
+				continue reapply
+			}
+
+			log.Info().Msgf("got a public config update: %+v", event.PublicConfig)
+			if err := setPublicConfig(ctx, cl, event.PublicConfig); err != nil {
+				return errors.Wrap(err, "failed to set public config")
+			}
+		}
+	}
+}
--- pkg/events/events.go
+++ pkg/events/events.go
@@ -1,1 +1,145 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/4ccaaa099b9ab98ae2217133a01cdfff
+package events
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/substrate-client"
+	"github.com/threefoldtech/zos/pkg"
+)
+
+var (
+	errStreamDisconnected = fmt.Errorf("stream disconnected")
+)
+
+type Manager struct {
+	mgr           substrate.Manager
+	node          uint32
+	pubCfg        chan pkg.PublicConfigEvent
+	contactCancel chan pkg.ContractCancelledEvent
+
+	o sync.Once
+}
+
+func New(mgr substrate.Manager, node uint32) pkg.Events {
+	return &Manager{
+		mgr:           mgr,
+		node:          node,
+		pubCfg:        make(chan pkg.PublicConfigEvent),
+		contactCancel: make(chan pkg.ContractCancelledEvent),
+	}
+}
+
+func (m *Manager) start(ctx context.Context) {
+	log.Info().Msg("start listening to chain events")
+	for {
+		if err := m.listen(ctx); err != nil {
+			log.Error().Err(err).Msg("listening to events failed, retry in 10 seconds")
+			<-time.After(10 * time.Second)
+		}
+	}
+}
+
+func (m *Manager) events(ctx context.Context) error {
+	subClient, meta, err := m.mgr.Raw()
+	if err != nil {
+		return errors.Wrap(err, "failed to get client to substrate")
+	}
+
+	defer subClient.Client.Close()
+
+	m.pubCfg <- pkg.PublicConfigEvent{Kind: pkg.EventSubscribed}
+	m.contactCancel <- pkg.ContractCancelledEvent{Kind: pkg.EventSubscribed}
+
+	// Subscribe to system events via storage
+	key, err := types.CreateStorageKey(meta, "System", "Events", nil)
+	if err != nil {
+		return errors.Wrap(err, "failed to get storage key for events")
+	}
+
+	reg, err := subClient.RPC.State.SubscribeStorageRaw([]types.StorageKey{key})
+	if err != nil {
+		return errors.Wrap(err, "failed to subscribe to events")
+	}
+
+	for {
+		select {
+		case event := <-reg.Chan():
+			//m.sub.GetBlock(block types.Hash)
+			for _, change := range event.Changes {
+				if !change.HasStorageData {
+					continue
+				}
+
+				var events substrate.EventRecords
+				if err := types.EventRecordsRaw(change.StorageData).DecodeEventRecords(meta, &events); err != nil {
+					log.Error().Err(err).Msg("failed to decode events from tfchain")
+					continue
+				}
+
+				for _, e := range events.TfgridModule_NodePublicConfigStored {
+					if e.Node != types.U32(m.node) {
+						continue
+					}
+					log.Info().Msgf("got a public config update: %+v", e.Config)
+					m.pubCfg <- pkg.PublicConfigEvent{
+						Kind:         pkg.EventReceived,
+						PublicConfig: e.Config,
+					}
+				}
+
+				for _, e := range events.SmartContractModule_NodeContractCanceled {
+					if e.Node != types.U32(m.node) {
+						continue
+					}
+					log.Info().Uint64("contract", uint64(e.ContractID)).Msg("got contract cancel update")
+					m.contactCancel <- pkg.ContractCancelledEvent{
+						Kind:     pkg.EventReceived,
+						Contract: uint64(e.ContractID),
+						TwinId:   uint32(e.Twin),
+					}
+				}
+			}
+		case err := <-reg.Err():
+			// need a reconnect
+			return errors.Wrapf(errStreamDisconnected, "subscription to events stopped (%s), reconnecting", err)
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+// Start subscribing and producing events
+func (m *Manager) listen(ctx context.Context) error {
+	for {
+		err := m.events(ctx)
+		if errors.Is(err, context.Canceled) {
+			return nil
+		} else if errors.Is(err, errStreamDisconnected) {
+			continue
+		} else {
+			return err
+		}
+	}
+}
+
+func (m *Manager) PublicConfigEvent(ctx context.Context) <-chan pkg.PublicConfigEvent {
+	m.o.Do(func() {
+		go m.start(ctx)
+	})
+
+	return m.pubCfg
+}
+
+func (m *Manager) ContractCancelledEvent(ctx context.Context) <-chan pkg.ContractCancelledEvent {
+	m.o.Do(func() {
+		go m.start(ctx)
+	})
+
+	return m.contactCancel
+}
