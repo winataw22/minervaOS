15fc80354050e87863da23bae4919c5cc40863bf
xmonader <xmonader@gmail.com>
2023-09-08 14:11:58 +0300

    Merge: Merge pull request #2051 from threefoldtech/xmonader-patch-1

--- pkg/network/iperf/iperf.go
+++ pkg/network/iperf/iperf.go
@@ -1,1 +1,42 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/c81bb27e3db8e4619d685a418c11b207
+package iperf
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/pkg/errors"
+	"github.com/threefoldtech/zos/pkg/zinit"
+)
+
+const (
+	zinitService = "iperf"
+	// IperfPort is the port for the iperf service
+	IperfPort = 300
+)
+
+// Ensure creates an iperf zinit service and monitors it
+func Ensure(z *zinit.Client) error {
+	if _, err := z.Status(zinitService); err == nil {
+		return nil
+	}
+
+	_, err := exec.LookPath("iperf")
+	if err != nil {
+		return err
+	}
+
+	cmd := fmt.Sprintf("ip netns exec public iperf -s -p %d", IperfPort)
+
+	err = zinit.AddService(zinitService, zinit.InitService{
+		Exec: cmd,
+		After: []string{
+			"networkd",
+		},
+	})
+
+	if err != nil {
+		return errors.Wrap(err, "failed to add iperf service")
+	}
+
+	return z.Monitor(zinitService)
+}
