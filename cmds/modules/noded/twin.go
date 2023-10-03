20130c3ea8ebe809a2136b8efe3b1df370390fd2
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2023-10-03 14:53:25 +0300

    Squashed 2 commits from 2023-10-03
    - adds flag to disable gpu (#2067)
    - adds disable-gpu docs (#2068)

--- cmds/modules/noded/twin.go
+++ cmds/modules/noded/twin.go
@@ -1,1 +1,89 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/deefa6c14591df363064326a37608750
+package noded
+
+import (
+	"context"
+	"crypto/ed25519"
+	"encoding/hex"
+	"fmt"
+	"net/url"
+	"os"
+	"os/exec"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/zos/pkg/kernel"
+)
+
+const (
+	keyType = "ed25519"
+)
+
+func withDefaultPort(substrateUrl string) (string, error) {
+	u, err := url.ParseRequestURI(substrateUrl)
+	if err != nil {
+		return "", err
+	}
+
+	if u.Port() != "" {
+		// already have the port
+		return substrateUrl, nil
+	}
+
+	if u.Scheme == "ws" {
+		u.Host += ":80"
+	} else {
+		u.Host += ":443"
+	}
+
+	return u.String(), nil
+}
+
+func runMsgBus(ctx context.Context, sk ed25519.PrivateKey, substrateURLs []string, relayAddr []string, redisAddr string) error {
+	// select the first one as only one URL is set for now
+	if len(substrateURLs) == 0 {
+		return errors.New("at least one substrate URL must be provided")
+	}
+
+	if len(relayAddr) == 0 {
+		return errors.New("at least one relay URL must be provided")
+	}
+
+	seed := sk.Seed()
+	seedHex := fmt.Sprintf("0x%s", hex.EncodeToString(seed))
+
+	log.Info().Msg("starting rmb...")
+
+	args := []string{
+		"-k", keyType,
+		"--seed", seedHex,
+		"--redis", redisAddr,
+	}
+
+	for _, url := range substrateURLs {
+		url, err := withDefaultPort(url)
+		if err != nil {
+			return err
+		}
+		args = append(args, "--substrate", url)
+	}
+
+	for _, url := range relayAddr {
+		args = append(args, "--relay", url)
+	}
+
+	if kernel.GetParams().IsDebug() {
+		args = append(args, "-d")
+	}
+
+	command := exec.CommandContext(
+		ctx,
+		"rmb",
+		args...,
+	)
+
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	log.Debug().Stringer("command", command).Msg("running rmb")
+	return command.Run()
+}
