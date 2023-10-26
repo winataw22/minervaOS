c0c505311a4de4c2621e3eb5ea110af885234fc6
Muhamad Azamy <muhamad@incubaid.com>
2023-10-26 10:50:54 +0200

    Squashed 4 commits from 2023-10-26
    - unify rfs and zufs pkgs (#2091)
    - Rework release (#2082)
    - the last .0 is importnat for version parsing
    - make sure nodes don't get into dead update lock

--- pkg/perf/cpubench/cpubench_task.go
+++ pkg/perf/cpubench/cpubench_task.go
@@ -1,1 +1,73 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/6ba944ca7c92f94a9b922dbd68914a07
+package cpubench
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+
+	"github.com/threefoldtech/zos/pkg/perf"
+	"github.com/threefoldtech/zos/pkg/stubs"
+)
+
+const cpuBenchmarkTaskID = "CPUBenchmark"
+const cpuBenchmarkCronSchedule = "0 0 */6 * * *"
+
+// CPUBenchmarkTask defines CPU benchmark task data.
+type CPUBenchmarkTask struct {
+	// taskID is a unique string ID for the task.
+	taskID string
+	// schedule is a 6 field cron schedule (unlike unix cron).
+	schedule string
+}
+
+// CPUBenchmarkResult holds CPU benchmark results with the workloads number during the benchmark.
+type CPUBenchmarkResult struct {
+	SingleThreaded float64 `json:"single"`
+	MultiThreaded  float64 `json:"multi"`
+	Threads        int     `json:"threads"`
+	Workloads      int     `json:"workloads"`
+}
+
+var _ perf.Task = (*CPUBenchmarkTask)(nil)
+
+// NewCPUBenchmarkTask returns a new CPU benchmark task.
+func NewCPUBenchmarkTask() CPUBenchmarkTask {
+	return CPUBenchmarkTask{
+		taskID:   cpuBenchmarkTaskID,
+		schedule: cpuBenchmarkCronSchedule,
+	}
+}
+
+// ID returns task ID.
+func (c *CPUBenchmarkTask) ID() string {
+	return c.taskID
+}
+
+// Cron returns task cron schedule.
+func (c *CPUBenchmarkTask) Cron() string {
+	return c.schedule
+}
+
+// Run executes the CPU benchmark.
+func (c *CPUBenchmarkTask) Run(ctx context.Context) (interface{}, error) {
+	cpubenchOut, err := exec.CommandContext(ctx, "cpubench", "-j").CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute cpubench command: %w", err)
+	}
+	cpuBenchmarkResult := CPUBenchmarkResult{}
+	err = json.Unmarshal(cpubenchOut, &cpuBenchmarkResult)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse cpubench output: %w", err)
+	}
+	client := perf.GetZbusClient(ctx)
+	statistics := stubs.NewStatisticsStub(client)
+
+	workloads, err := statistics.Workloads(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get workloads number: %w", err)
+	}
+
+	cpuBenchmarkResult.Workloads = workloads
+	return cpuBenchmarkResult, nil
+}
--- pkg/perf/iperf/iperf_task.go
+++ pkg/perf/iperf/iperf_task.go
@@ -1,1 +1,160 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/6a1dcb3695960a2bf9a668af86f215cd
+package iperf
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/zos/pkg/environment"
+	"github.com/threefoldtech/zos/pkg/network/iperf"
+	"github.com/threefoldtech/zos/pkg/perf"
+)
+
+// IperfTest for iperf tcp/udp tests
+type IperfTest struct {
+	taskID   string
+	schedule string
+}
+
+// IperfResult for iperf test results
+type IperfResult struct {
+	UploadSpeed   float64               `json:"upload_speed"`   // in bit/sec
+	DownloadSpeed float64               `json:"download_speed"` // in bit/sec
+	NodeID        uint32                `json:"node_id"`
+	NodeIpv4      string                `json:"node_ip"`
+	TestType      string                `json:"test_type"`
+	Error         string                `json:"error"`
+	CpuReport     CPUUtilizationPercent `json:"cpu_report"`
+}
+
+// NewTask creates a new iperf test
+func NewTask() perf.Task {
+	// because go-iperf left tmp directories with perf binary in it each time
+	// the task had run
+	matches, _ := filepath.Glob("/tmp/goiperf*")
+	for _, match := range matches {
+		os.RemoveAll(match)
+	}
+	return &IperfTest{taskID: "iperf", schedule: "0 0 */6 * * *"}
+}
+
+// ID returns the ID of the tcp task
+func (t *IperfTest) ID() string {
+	return t.taskID
+}
+
+// Cron returns the schedule for the tcp task
+func (t *IperfTest) Cron() string {
+	return t.schedule
+}
+
+// Run runs the tcp test and returns the result
+func (t *IperfTest) Run(ctx context.Context) (interface{}, error) {
+	env := environment.MustGet()
+	g := NewGraphQl(env.GraphQL)
+
+	// get nodes
+	freeFarmNodes, err := g.ListPublicNodes(ctx, 0, 1, 0, true, true)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to list freefarm nodes from graphql")
+	}
+
+	nodes, err := g.ListPublicNodes(ctx, 12, 0, 1, true, true)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to list random nodes from graphql")
+	}
+
+	nodes = append(nodes, freeFarmNodes...)
+
+	_, err = exec.LookPath("iperf")
+	if err != nil {
+		return nil, err
+	}
+
+	var results []IperfResult
+	for _, node := range nodes {
+		clientIP, _, err := net.ParseCIDR(node.PublicConfig.Ipv4)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to parse ipv4 address")
+			continue
+		}
+
+		clientIPv6, _, err := net.ParseCIDR(node.PublicConfig.Ipv6)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to parse ipv6 address")
+			continue
+		}
+
+		// TCP
+		res := t.runIperfTest(ctx, clientIP.String(), true)
+		res.NodeID = node.NodeID
+		results = append(results, res)
+
+		res = t.runIperfTest(ctx, clientIPv6.String(), true)
+		res.NodeID = node.NodeID
+		results = append(results, res)
+
+		// UDP
+		res = t.runIperfTest(ctx, clientIP.String(), false)
+		res.NodeID = node.NodeID
+		results = append(results, res)
+
+		res = t.runIperfTest(ctx, clientIPv6.String(), false)
+		res.NodeID = node.NodeID
+		results = append(results, res)
+	}
+
+	return results, nil
+}
+
+func (t *IperfTest) runIperfTest(ctx context.Context, clientIP string, tcp bool) IperfResult {
+	opts := make([]string, 0)
+	opts = append(opts,
+		"--client", clientIP,
+		"--bandwidth", "1M",
+		"--port", fmt.Sprint(iperf.IperfPort),
+		"--interval", "20",
+		"--json",
+	)
+
+	if !tcp {
+		opts = append(opts, "--length", "16B", "--udp")
+	}
+	output, err := exec.CommandContext(ctx, "iperf", opts...).CombinedOutput()
+	exitErr := &exec.ExitError{}
+	if err != nil && !errors.As(err, &exitErr) {
+		log.Err(err).Msg("failed to run iperf")
+		return IperfResult{}
+	}
+
+	var report iperfCommandOutput
+	if err := json.Unmarshal(output, &report); err != nil {
+		log.Err(err).Msg("failed to parse iperf output")
+		return IperfResult{}
+	}
+
+	proto := "tcp"
+	if !tcp {
+		proto = "udp"
+	}
+	iperfResult := IperfResult{
+		UploadSpeed:   report.End.SumSent.BitsPerSecond,
+		DownloadSpeed: report.End.SumReceived.BitsPerSecond,
+		CpuReport:     report.End.CPUUtilizationPercent,
+		NodeIpv4:      clientIP,
+		TestType:      proto,
+		Error:         report.Error,
+	}
+
+	if !tcp && len(report.End.Streams) > 0 {
+		iperfResult.DownloadSpeed = report.End.Streams[0].UDP.BitsPerSecond
+	}
+
+	return iperfResult
+}
