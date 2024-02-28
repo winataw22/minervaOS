6b5f7e9c8b718ffb104f625b9e4ccc58b9cddfb1
Eslam Nawara <67752395+Eslam-Nawara@users.noreply.github.com>
2024-02-28 13:14:19 +0200

    use mocking for boot testing (#2201)
    * use mocking in boot.go
    
    * remove function receiver fs and use function arguments to pass bootedPath and fs
    
    * add boot.go tests
    
    * add tests for flag.go
    
    * add tests for cache.go
    
    * add docs to the created tests
    
    * add tests to logger
    
    * remove logger tests
    
    * move DefaultSystemOS implementation to pkg to be able to reuse the same implementation
    
    * move mock to os.go
    
    * implement `fileInfo` struct to use as Stat return

---
+++ pkg/app/boot_test.go
@@ -0,0 +1,1 @@
+/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/480dcbb95b221e1f4503ddef89e70b64
---
+++ pkg/cache/cache_test.go
@@ -0,0 +1,1 @@
+/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/c35bb335f006d3f5340bafd23304cd0b
