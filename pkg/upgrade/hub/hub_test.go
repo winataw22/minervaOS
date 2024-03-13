15ee78c3053ee887a9e273984c24cec0cd7c4867
Muhamad Awad <muhamad@incubaid.com>
2024-03-13 11:01:45 +0100

    only run gatewayd after networkd is running (#2231)
    Fixes #2230

--- pkg/upgrade/hub/hub_test.go
+++ pkg/upgrade/hub/hub_test.go
@@ -1,1 +1,40 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/b56a2ea6384373a46e6e0722626af950
+package hub
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHub(t *testing.T) {
+	const tag = "3b51aa5"
+	const repo = "tf-autobuilder"
+
+	hub := NewHubClient(defaultHubCallTimeout)
+
+	results, err := hub.ListTag(repo, tag)
+	require.NoError(t, err)
+	require.Len(t, results, 20)
+
+	var zos Symlink
+	for _, f := range results {
+		if f.Name == "zos.flist" {
+			zos = f
+			break
+		}
+	}
+
+	// symlink starts from repo, and can point us to
+	// another repo destRepo
+	destRepo, name, err := zos.Destination(repo)
+	require.NoError(t, err)
+
+	info, err := hub.Info(destRepo, name)
+	require.NoError(t, err)
+
+	regular := NewRegular(info)
+
+	files, err := regular.Files(repo)
+	require.NoError(t, err)
+	require.NotEmpty(t, files)
+}
